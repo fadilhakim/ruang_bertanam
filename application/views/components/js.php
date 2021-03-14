94a235c483f5805cb668e3109b851cac661db372
Fadil <fadil.nylon@gmail.com>
2021-03-14 21:09:57 +0700

    initial commit bismillah

---
+++ application/views/components/js.php
@@ -0,0 +1,17 @@
+<!-- Scripts -->
+<script src="https://maps.googleapis.com/maps/api/js"></script>
+<script src="<?=base_url("assets/js/jquery-2.1.3.min.js")?>"></script>
+<script src="<?=base_url("assets/js/bootstrap.js")?>"></script>
+<script src="<?=base_url("assets/js/plugins/superfish.min.js")?>"></script>
+<script src="<?=base_url("assets/js/jquery.ui.min.js")?>"></script>
+<script src="<?=base_url("assets/js/plugins/rangeslider.min.js")?>"></script>
+<script src="<?=base_url("assets/js/plugins/jquery.flexslider-min.js")?>"></script>
+<script src="<?=base_url("assets/js/uou-accordions.js")?>"></script>
+<script src="<?=base_url("assets/js/uou-tabs.js")?>"></script>
+<script src="<?=base_url("assets/js/plugins/select2.min.js")?>"></script>
+<script src="<?=base_url("assets/js/owl.carousel.min.js")?>"></script>
+<script src="<?=base_url("assets/js/gmap3.min.js")?>"></script>
+<script src="<?=base_url("assets/js/scripts.js")?>"></script>
+<script>
+
+</script>
